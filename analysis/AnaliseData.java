ec05c39258d39f11f2e2ee9ebedafbde6debcb72
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2022-01-13 17:23:18 +0200

    add int to analise

--- analysis/AnaliseData.java
+++ analysis/AnaliseData.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class AnaliseData
 {  
   public static void main(String[] args) {  
+    int cycle, countPeriod, amplitude;
     String line = "";  
     String splitBy = ";";  
 
@@ -12,7 +13,17 @@ public class AnaliseData
       BufferedReader bufferedReader = new BufferedReader(new FileReader("data.csv")); 
       while ((line = bufferedReader.readLine()) != null) { 
         String[] data = line.split(splitBy); 
-        System.out.println("Data: [Cycle = " + data[2] + ", count Period = " + data[4] + ", amplitude = " + data[6] + ", srt = " + data[8] +"]");  
+//        System.out.println("Data: [Cycle = " + data[2] + ", count Period = " + data[4] + ", amplitude = " + data[6] + ", srt = " + data[8] +"]");  
+        try {
+          cycle = Integer.parseInt(data[2]);
+          countPeriod = Integer.parseInt(data[4]);
+          amplitude = Integer.parseInt(data[4]);
+
+          System.out.printf("%d;%d;%d%n", cycle, countPeriod, amplitude);
+        }
+        catch (NumberFormatException ex) {
+          ex.printStackTrace();
+        }
       } 
       bufferedReader.close(); 
     } 
